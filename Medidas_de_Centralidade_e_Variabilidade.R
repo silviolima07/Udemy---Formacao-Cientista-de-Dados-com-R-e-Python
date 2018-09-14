3ae26118af08fc7cb1f48e115adacaaedcccd26b
SilvioLima <silviolima07@gmail.com>
2018-09-14 18:57:08 -0300

    Comandos: mean, median, quartis,sd, summary

--- Medidas_de_Centralidade_e_Variabilidade.R
+++ Medidas_de_Centralidade_e_Variabilidade.R
@@ -1,10 +1,14 @@
 # Medidas de Centralidade e Variabilidade
 #
 jogadores = c(40000,18000, 12000, 250000,30000,140000, 300000, 40000, 800000)
+# Média
 mean(jogadores)
+# Mediana
 median(jogadores)
+# Quartis
 quartis = quantile(jogadores)
 quartis[4]
+# Desvio padrao
 sd(jogadores)
 summary(jogadores)
 quartis
